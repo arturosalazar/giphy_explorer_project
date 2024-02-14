a1cfe755e5dd2376e121187841d47c61bcf0cb44
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2024-02-13 21:22:07 -0600

    Add script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+//Simple hello world to make sure the html and script are working together
+let heading = document.createElement("h1");
+heading.innerHTML = "Hello, World!";
+document.body.appendChild(heading);
